89a0301ec9c579f14958cb441303f597d1b1cd2a
tatamiya <tamyuji1002@gmail.com>
2021-06-28 22:39:39 +0900

    Server: The HTTP Server

---
+++ server/src/main.rs
@@ -0,0 +1,30 @@
+use actix_web::{rt::System, web, App, HttpResponse, HttpServer};
+use std::sync::mpsc;
+use std::thread;
+
+#[actix_web::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let sys = System::new("http-server");
+
+        let srv = HttpServer::new(|| {
+            App::new().route("/", web::get().to(|| HttpResponse::Ok()))
+        })
+        .bind("127.0.0.1:8080")?
+        .shutdown_timeout(60)
+        .run();
+
+        let _ = tx.send(srv);
+        sys.run()
+    });
+
+    let srv = rx.recv().unwrap();
+
+    srv.pause().await;
+    srv.resume().await;
+    srv.stop(true).await;
+
+}
+
