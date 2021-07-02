69593f8ba46a17793c22d1fb7223b6d59d4be0ec
tatamiya <tamyuji1002@gmail.com>
2021-07-03 00:07:57 +0900

    Errors: Recommended practices in error handling

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -60,6 +60,30 @@ async fn error_helpers() -> Result<&'static str> {
     Ok(result.map_err(|e| error::ErrorBadRequest(e.name))?)
 }
 
+#[derive(Debug, Display, Error)]
+enum UserError {
+    #[display(fmt = "Validation error on field: {}", field)]
+    ValidationError { field: String },
+
+    #[display(fmt = "an internal error occurred. Please try again later.")]
+    InternalError,
+}
+
+impl error::ResponseError for UserError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.status_code())
+        .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+        .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            UserError::ValidationError { .. } => StatusCode::BAD_REQUEST,
+            UserError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
