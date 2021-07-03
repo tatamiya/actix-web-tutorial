06c251c114977e1732ea382e47cdd8f7cd34e6e4
tatamiya <tamyuji1002@gmail.com>
2021-07-03 18:46:32 +0900

    URL Dispatch: Match information

--- url_dispatch/src/main.rs
+++ url_dispatch/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{guard, get, web, App, HttpResponse, HttpServer};
+use actix_web::{guard, get, web, App, HttpRequest, HttpResponse, HttpServer, Result};
 
 async fn hello() -> HttpResponse {
     HttpResponse::Ok().body("Hello")
@@ -13,6 +13,14 @@ async fn show_users() -> HttpResponse {
 #[get("/show/{id}")]
 async fn user_detail(path: web::Path<(u32, )>) -> HttpResponse {
     HttpResponse::Ok().body(format!("User detail: {}", path.into_inner().0))
+}
+
+#[get("/match_information/{v1}/{v2}")]
+async fn match_information(req: HttpRequest) -> Result<String> {
+    let v1: u8 = req.match_info().get("v1").unwrap().parse().unwrap();
+    let v2: u8 = req.match_info().query("v2").parse().unwrap();
+    let (v3, v4): (u8, u8) = req.match_info().load().unwrap();
+    Ok(format!("Values {} {} {} {}", v1, v2, v3, v4))
 }
 
 #[actix_web::main]
@@ -33,6 +41,7 @@ async fn main() -> std::io::Result<()> {
                     .service(show_users)
                     .service(user_detail)
             )
+            .service(match_information)
     })
     .bind("127.0.0.1:8080")?
     .run()
