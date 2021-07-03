a6e9d163f4dbce38c30a94085e2cca7732f8cf6d
tatamiya <tamyuji1002@gmail.com>
2021-07-03 18:40:09 +0900

    URL Dispatch: Scoping Routes

--- url_dispatch/src/main.rs
+++ url_dispatch/src/main.rs
@@ -1,7 +1,18 @@
-use actix_web::{guard, web, App, HttpResponse, HttpServer};
+use actix_web::{guard, get, web, App, HttpResponse, HttpServer};
 
 async fn hello() -> HttpResponse {
     HttpResponse::Ok().body("Hello")
+}
+
+
+#[get("/show")]
+async fn show_users() -> HttpResponse {
+    HttpResponse::Ok().body("Show users")
+}
+
+#[get("/show/{id}")]
+async fn user_detail(path: web::Path<(u32, )>) -> HttpResponse {
+    HttpResponse::Ok().body(format!("User detail: {}", path.into_inner().0))
 }
 
 #[actix_web::main]
@@ -16,6 +27,11 @@ async fn main() -> std::io::Result<()> {
                     .guard(guard::Header("content-type", "application/json"))
                     .route(web::get().to(|| HttpResponse::Ok()))
                     .route(web::put().to(|| HttpResponse::Ok()))
+            )
+            .service(
+                web::scope("/scope")
+                    .service(show_users)
+                    .service(user_detail)
             )
     })
     .bind("127.0.0.1:8080")?
