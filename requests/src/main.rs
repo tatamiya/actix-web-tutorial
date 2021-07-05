f9d0ab430060570beb2adc73a5e49d73e3e9ab98
tatamiya <tamyuji1002@gmail.com>
2021-07-05 23:57:33 +0900

    Requests: JSON Request

---
+++ requests/src/main.rs
@@ -0,0 +1,48 @@
+use actix_web::{error, web, post, App, HttpServer, HttpResponse, Result, Error};
+use futures::StreamExt;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct Info {
+    username: String,
+}
+
+async fn json_request(info: web::Json<Info>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.username))
+}
+
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+
+const MAX_SIZE: usize = 262_144;
+
+#[post("/load_and_deserialize")]
+async fn load_and_deserialize(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+
+    let obj = serde_json::from_slice::<MyObj>(&body)?;
+    Ok(HttpResponse::Ok().json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||
+        App::new()
+            .route("/json_request", web::post().to(json_request))
+            .service(load_and_deserialize)
+    )
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
