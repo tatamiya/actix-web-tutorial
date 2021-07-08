741414bc1f098c5aa18916380e84c04d5e3ec7dc
tatamiya <tamyuji1002@gmail.com>
2021-07-08 23:54:29 +0900

    Testing: Stream response tests

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,4 +1,11 @@
-use actix_web::{HttpResponse, HttpRequest, http::ContentEncoding, dev::BodyEncoding};
+use futures::stream::poll_fn;
+use std::task::Poll;
+
+use actix_web::http::{ContentEncoding, StatusCode};
+
+use actix_web::{
+    http, web, App, Error, HttpResponse, HttpRequest, dev::BodyEncoding
+};
 
 pub async fn index(_req: HttpRequest) -> HttpResponse {
 
@@ -7,8 +14,30 @@ pub async fn index(_req: HttpRequest) -> HttpResponse {
         .body("data")
 }
 
+
+async fn sse(_req: HttpRequest) -> HttpResponse {
+    let mut counter: usize = 5;
+
+    let server_events = poll_fn(move |_cx| -> Poll<Option<Result<web::Bytes, Error>>> {
+        if counter == 0 {
+            return Poll::Ready(None);
+        }
+        let payload = format!("data: {}\n\n", counter);
+        counter -= 1;
+        Poll::Ready(Some(Ok(web::Bytes::from(payload))))
+    });
+
+    HttpResponse::build(StatusCode::OK)
+        .set_header(http::header::CONTENT_TYPE, "text/event-stream")
+        .set_header(
+            http::header::CONTENT_ENCODING,
+            ContentEncoding::Identity.as_str(),
+        )
+        .streaming(server_events)
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new().route("/", web::get().to(sse));
 }
 
 #[cfg(test)]
@@ -51,5 +80,44 @@ mod integration_tests {
         let req = test::TestRequest::post().uri("/").to_request();
         let resp = test::call_service(&mut app, req).await;
         assert!(resp.status().is_client_error());
+    }
+}
+
+#[cfg(test)]
+mod stream_response_tests {
+    use super::*;
+    use futures_util::stream::StreamExt;
+    use futures_util::stream::TryStreamExt;
+
+    use actix_web::{test, web, App};
+
+    #[actix_rt::test]
+    async fn test_stream() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(sse))).await;
+        let req = test::TestRequest::get().to_request();
+
+        let mut resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+
+        // first chunk
+        let (bytes, mut resp) = resp.take_body().into_future().await;
+        assert_eq!(
+            bytes.unwrap().unwrap(),
+            web::Bytes::from_static(b"data: 5\n\n")
+        );
+
+        // second chunk
+        let (bytes, mut resp) = resp.take_body().into_future().await;
+        assert_eq!(
+            bytes.unwrap().unwrap(),
+            web::Bytes::from_static(b"data: 4\n\n")
+        );
+
+        // remaining part
+        let bytes = test::load_stream(resp.take_body().into_stream()).await;
+        assert_eq!(
+            bytes.unwrap(),
+            web::Bytes::from_static(b"data: 3\n\ndata: 2\n\ndata: 1\n\n")
+        );
     }
 }
