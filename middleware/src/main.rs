3bab467410e2ebe36ef18b13b3406d43573a7cc6
tatamiya <tamyuji1002@gmail.com>
2021-07-10 15:44:52 +0900

    Middleware: Error handlers

--- middleware/src/main.rs
+++ middleware/src/main.rs
@@ -2,9 +2,12 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use actix_service::{Service, Transform};
+
 use actix_session::{CookieSession, Session};
-use actix_web::{http, web, App, dev::ServiceRequest, dev::ServiceResponse, Error, HttpResponse, HttpServer};
-use actix_web::middleware::{Logger, DefaultHeaders};
+
+use actix_web::{http, web, App, dev::ServiceRequest, dev::ServiceResponse, Error, HttpResponse, HttpServer, Result};
+use actix_web::middleware::{Logger, DefaultHeaders, errhandlers::ErrorHandlerResponse, errhandlers::ErrorHandlers};
+
 use env_logger::Env;
 use futures::future::{ok, Ready, FutureExt};
 use futures::Future;
@@ -75,6 +78,14 @@ async fn user_session(session: Session) -> Result<HttpResponse, Error> {
     )))
 }
 
+fn render_500<B>(mut res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    res.response_mut().headers_mut().insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("Error"),
+    );
+    Ok(ErrorHandlerResponse::Response(res))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
@@ -112,6 +123,15 @@ async fn main() -> std::io::Result<()> {
                     .secure(false),
             )
             .service(web::resource("/user_session").to(user_session))
+            .wrap(
+                ErrorHandlers::new()
+                    .handler(http::StatusCode::INTERNAL_SERVER_ERROR, render_500),
+            )
+            .service(
+                web::resource("/error_handler")
+                    .route(web::get().to(|| HttpResponse::Ok()))
+                    .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+            )
         )
         .bind("127.0.0.1:8080")?
         .run()
