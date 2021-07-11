71611e51854b522d7e533caa894d33e588f6005b
tatamiya <tamyuji1002@gmail.com>
2021-07-11 14:05:25 +0900

    Static Files: Configuration

--- static_files/src/main.rs
+++ static_files/src/main.rs
@@ -1,11 +1,24 @@
 use actix_files as fs;
-use actix_files::NamedFile;
-use actix_web::{HttpRequest, Result};
+use actix_web::{get, HttpRequest, Result, Error};
+use actix_web::http::header::{ContentDisposition, DispositionType};
 use std::path::PathBuf;
 
-async fn individual_file(req: HttpRequest) -> Result<NamedFile> {
+async fn individual_file(req: HttpRequest) -> Result<fs::NamedFile> {
     let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    Ok(NamedFile::open(path)?)
+    Ok(fs::NamedFile::open(path)?)
+}
+
+#[get("/configuration/{filename:.*}")]
+async fn configuration(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let file = fs::NamedFile::open(path)?;
+    Ok(file
+        .use_last_modified(true)
+        .set_content_disposition(ContentDisposition {
+            disposition: DispositionType::Attachment,
+            parameters: vec![],
+        })
+    )
 }
 
 #[actix_web::main]
@@ -15,8 +28,13 @@ async fn main() -> std::io::Result<()>{
     HttpServer::new(||
         App::new()
             .route("/individual_file/{filename:.*}", web::get().to(individual_file))
-            //.service(fs::Files::new("/static", ".").show_files_listing())
-            .service(fs::Files::new("/static", ".").index_file("index.html"))
+            .service(
+                fs::Files::new("/static", ".")
+                    .show_files_listing()
+                    .use_last_modified(true),
+            )
+            //.service(fs::Files::new("/static", ".").index_file("index.html"))
+            .service(configuration)
     )
     .bind("127.0.0.1:8080")?
     .run()
