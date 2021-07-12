eaf347d88e463ddcba66041e3256c3ea74a7da06
tatamiya <tamyuji1002@gmail.com>
2021-07-12 23:54:22 +0900

    Databases: schema, models, actions

---
+++ databases/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate diesel;
+
+use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+use uuid::Uuid;
+
+mod actions;
+mod models;
+mod schema;
+
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+fn main() {
+    println!("Hello, world!");
+}
